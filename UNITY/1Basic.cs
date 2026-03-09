96de83c89f25eac5026513cee0009229c307c274
peehultina23 <peehulpradhan23bsd027@gmail.com>
2026-03-09 22:01:02 +0530

    Add new scripts for rotation, translation, and scaling in Unity

--- UNITY/1Basic.cs
+++ UNITY/1Basic.cs
@@ -1,3 +1,6 @@
+New 2D project > Game Object > 2D object > Sprites > Circle
+Add component > new script
+
 
 using UnityEngine;
 public class BasicScript : MonoBehaviour
@@ -168,3 +171,135 @@ class Program
     }
 }
 
+#sakina code
+//using if else statement to find whether the no is odd or not
+
+using UnityEngine;
+public class practical: MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        evennum();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void evennum()
+    {
+        int number = 2;
+        if(number % 2 == 0)
+        {
+            Debug.Log(number + " is Even");
+        }
+        else
+        {
+            Debug.Log(number + " is Odd");
+        }
+    }
+}
+
+// using switch acse to calculate grade of student
+
+using UnityEngine;
+public class practical: MonoBehaviour
+{
+    void Start()
+    {
+        switchc();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void switchc()
+    {
+        int marks = 85;
+        char grade;
+        switch(marks)
+        {
+            case int m when(m >= 90):
+                grade = 'A';
+                break;
+            case int m when(m >= 80):
+                grade = 'B';
+                break;
+            case int m when(m >= 70):
+                grade = 'C';
+                break;
+            case int m when(m >= 60):
+                grade = 'D';
+                break;
+            default:
+                grade = 'F';
+                break;
+        }
+        Debug.Log("Grade: " + grade);
+    }
+}
+
+//using for loop find sum of n numbers
+
+using UnityEngine;
+public class practical: MonoBehaviour
+{
+    // Start is called once before the first execution of Update
+    void Start()
+    {
+        forloo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void forloo()
+    {
+        int n = 5;
+        int sum = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+        Debug.Log("Sum of first " + n + " numbers = " + sum);
+    }
+}
+
+// using while loop to find sum of all even number in a given range
+
+using UnityEngine;
+public class practical: MonoBehaviour
+{
+    // Start is called once before the first execution of Update
+    void Start()
+    {
+        whileloo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void whileloo()
+    {
+        int limit = 50;
+        int sumEven = 0;
+        int i = 0;
+        while(i <= limit)
+        {
+            if(i % 2 == 0)
+            {
+                sumEven += i;
+            }
+            i++;
+        }
+        Debug.Log("Sum of all even numbers from 0 to 50 = " + sumEven);
+    }
+}
--- UNITY/2Translation_Rotation_Scaling.cs
+++ UNITY/2Translation_Rotation_Scaling.cs
@@ -1,3 +1,8 @@
+New 2D project > Game Object > 2D object > Sprites > Circle
+Components > Physics2D > Rigidbody 2D
+Go to inspector > Body type = ‘Dynamic’> Gravity scale = ‘0’
+Edit > Project setting > Player > Other settings > Active input handling = ‘Both’
+Add component > new script
 
 using UnityEngine;
 public class TransformController : MonoBehaviour
@@ -84,5 +89,69 @@ public class TransformController : MonoBehaviour
                 Mathf.Max(transform.localScale.y, 0.1f),
                 1);
         }
+    }
+}
+
+#sakina code
+//Rotation
+
+using UnityEngine;
+public class Rotate: MonoBehaviour
+{
+    public float rotationSpeed = 150f; // speed of player rotation
+    void Update()
+    {
+        float rotateInput = Input.GetAxis("Horizontal"); 
+        // A = -1, D = +1
+        transform.Rotate(0, 0, rotateInput * rotationSpeed * Time.deltaTime);
+    }
+}
+
+//Translation
+
+using UnityEngine;
+public class Translation: MonoBehaviour
+{
+    public float speed = 1f; // speed of player
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal"); // A/D or Left/Right arrows
+        float moveY = Input.GetAxis("Vertical");   // W/S or Up/Down arrows
+        Vector2 movement = new Vector2(moveX, moveY);
+        rb2d.linearVelocity = movement * speed;
+    }
+}
+
+// Scaling
+
+using UnityEngine;
+public class Scaling: MonoBehaviour
+{
+    public float scaleSpeed = 1f;
+    public float minScale = 0.2f;
+    public float maxScale = 2f;
+
+    void Update()
+    {
+        float scaleInput = Input.GetAxis("Vertical");
+        // Create a new scale based on input
+        Vector3 newScale = transform.localScale + Vector3.one * scaleInput * scaleSpeed * Time.deltaTime;
+
+        // Clamp X and Y scale
+        newScale.x = Mathf.Clamp(newScale.x, minScale, maxScale);
+        newScale.y = Mathf.Clamp(newScale.y, minScale, maxScale);
+
+        // Force Z scale to stay 1
+        newScale.z = 1f;
+
+        // Apply the scale
+        transform.localScale = newScale;
     }
 }
