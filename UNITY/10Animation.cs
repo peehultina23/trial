ff638cf8837e318c0c4d63274db712fb5b111a27
peehultina23 <peehulpradhan23bsd027@gmail.com>
2026-03-10 00:50:16 +0530

    Final Animation

--- UNITY/10Animation.cs
+++ UNITY/10Animation.cs
@@ -37,7 +37,42 @@ public class Animation_Script : MonoBehaviour
 7. Copy and paste the above code into the Animation_Script
 8. Run the scene and press the W key to make the character walk and the D key to make it run.
 
-#inspector window:
-- Animator component: assign the animator controller to the Controller field
-- Animation_Script component: no additional settings needed, as the script will automatically reference the animator component and respond to the W and D key inputs to control the walking and running animations.
+Phase 1: Asset Preparation (Mixamo)
+ * Character: Download a character (e.g., "Arissa") as FBX Binary with Skin.
+ * Animations: Search and download Running, Breathing, and Walking animations.
+   * Settings: 30 FPS, Without Skin (since you already have the character skin).
+ * Unity Import: Create an "animation" folder in your Assets and drag all FBX files into it.
+ * Fix Textures: Select the character FBX in Unity, go to the Materials tab in the Inspector, and click Extract Textures and Extract Materials.
 
+Phase 2: Animator Controller Setup
+ * Create Controller: Right-click in your folder > Create > Animator Controller. Name it "animator".
+ * Add States: Open the Animator window and drag your animation clips (breathing, walking, running) into the grid.
+ * Define Parameters: In the Animator window, click the Parameters tab, click +, select Bool, and create two: Walk and Running.
+ * Set Transitions: * Right-click breathing > Make Transition to walking (and vice versa).
+   * Right-click breathing > Make Transition to running (and vice versa).
+ * Configure Logic: Click each transition arrow and uncheck Has Exit Time. Add Conditions:
+   * Breathing \rightarrow Walking: Walk = true.
+   * Walking \rightarrow Breathing: Walk = false.
+   * Breathing \rightarrow Running: Running = true.
+   * Running \rightarrow Breathing: Running = false.
+
+Phase 3: Scripting & Binding
+ * Create Script: Create Animation_script.cs and use the following logic:
+ 
+   public class Animation_script : MonoBehaviour {
+    private Animator myanim;
+    void Start() { myanim = GetComponent<Animator>(); [span_23](start_span)} //[span_23](end_span)
+    void Update() {
+        myanim.SetBool("Walk", Input.GetKey(KeyCode.W)); [span_24](start_span)//[span_24](end_span)
+        myanim.SetBool("Running", Input.GetKey(KeyCode.D)); [span_25](start_span)//[span_25](end_span)
+    }
+}
+
+ * Attach Components: Select your character in the Hierarchy:
+   * Drag the Animator Controller into the Animator component's Controller slot.
+   * Drag the Animation_script onto the character to add it as a component.
+
+Phase 4: Testing
+ * Press Play in Unity.
+ * Hold W to trigger the Walking animation.
+ * Hold D to trigger the Running animation.
